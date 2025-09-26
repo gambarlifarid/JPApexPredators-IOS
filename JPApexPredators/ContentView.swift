db0e88b4d160df892a9aa1212ab2ca8e45765d7e
gambarlifarid <ferid.qemberli06@gmail.com>
2025-09-27 01:16:35 +0400

    Initial Commit

---
+++ JPApexPredators/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  JPApexPredators
+//
+//  Created by Farid Gambarli on 28.06.25.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+    let predators = Predators()
+    
+    @State var searchText = ""
+    @State var alphabetical = false
+    @State var currenctSelection = APType.all
+    
+    var filteredDinos: [ApexPredator] {
+        predators.filter(by: currenctSelection)
+        
+        predators.sort(by: alphabetical)
+        
+        return predators.search(for: searchText)
+    }
+    
+    var body: some View {
+        NavigationStack {
+            List(filteredDinos) { predator in
+                NavigationLink {
+                    PredatorDetail(predator: predator, position: .camera(
+                        MapCamera(
+                            centerCoordinate: predator.location,
+                            distance: 30000
+                        )))
+                } label: {
+            
+                    HStack {
+                        // Dinosaur image
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .white, radius: 1)
+                        
+                        VStack(alignment: .leading) {
+                            // Name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            
+                            // Type
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Apex Predators")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        withAnimation {
+                            alphabetical.toggle()
+                        }
+                    } label: {
+                        Image(systemName: alphabetical ? "film" : "textformat")
+                            .symbolEffect(.bounce, value: alphabetical)
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu {
+                        Picker("Filter", selection: $currenctSelection.animation()) {
+                            ForEach(APType.allCases) { type in
+                                Label(type.rawValue.capitalized, systemImage: type.icon)
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "slider.horizontal.3")
+                    }
+                }
+            }
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Farid Gambarli on 28.06.25.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
